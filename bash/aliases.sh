0bcf594166d3698db7afb0bf75c0ba4b60ab0f83
Héctor Climente <hclimente@users.noreply.github.com>
2016-05-16 23:04:49 +0200

    more aliases
    added aliases from http://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,1 +1,20 @@
-alias grep="grep --colo" r
+
+alias grep="grep --color"
+
+# a quick way to get out of current directory
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+alias h='history'
+
+# do not delete / or prompt if deleting more than 3 files at a time #
+alias rm='rm -I --preserve-root'
+ 
+# confirmation #
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
