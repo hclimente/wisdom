6ae21ec44bce8503eb33e6d44ec40cd44caabb37
Héctor Climente <hclimente@gmail.com>
2016-08-02 16:45:57 +0200

    added data_analysis environment

---
+++ r/data_analysis_environment.R
@@ -0,0 +1,11 @@
+# convenient for reading data
+library(readr)
+
+# faster, more comfortable data frames
+library(data.table)
+
+# clean data easily
+library(tidyr)
+
+# manipulate data
+library(dplyr)
