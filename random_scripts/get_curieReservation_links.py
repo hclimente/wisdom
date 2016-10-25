59ff6dc06bf2ffe4a0b1345ef786631386155248
Hector Climente <hclimente@gmail.com>
2016-10-25 16:43:10 +0200

    code documented

--- random_scripts/get_curieReservation_links.py
+++ random_scripts/get_curieReservation_links.py
@@ -6,6 +6,8 @@ import argparse
 
 now = datetime.datetime.now()
 
+# parse the desired timeframe
+## define parser
 parser = argparse.ArgumentParser(description='Get links for reserving rooms at curie')
 parser.add_argument('-d', '--day', dest='weekday', type=int, default = 2, help='Index of the name of the week (Monday = 1; Sunday = 7)')
 parser.add_argument('-m', '--first_month', dest='first_month', type=int, default = now.month, help='Index of the starting month (January = 1; December = 12).')
@@ -13,13 +15,16 @@ parser.add_argument('-l', '--last_month', dest='last_month', type=int, default =
 parser.add_argument('-y', '--year', dest='year', type=int, default = now.year, help='Year.')
 args = parser.parse_args()
 
-baseURL = 'http://ads.curie.fr/cgi-bin/reservations/grille.sh?resa+'
-
+## retrieve the values
 year = args.year
 firstMonth = args.first_month
 lastMonth = args.last_month + 1
 weekday = args.weekday - 1
 
+# define URL related variables
+baseURL = 'http://ads.curie.fr/cgi-bin/reservations/grille.sh?resa+'
+
+## rooms ranked by order of preference
 rooms = [
     # tier 1
     "lacassagne", "curie", "lhomond",
@@ -32,10 +37,13 @@ rooms = [
     # tier 5 salle25u6
     "salle25u6"]
 
+## month and weekdays in french
 frMonths = ["Janvier", "F%E9vrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Ao%FBt", "Septembre", "Octobre", "Novembre", "D%E9cembre"]
 frWeekdays = [ "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" ]
 frWeekday = frWeekdays[weekday]
 
+# define a counter to get a unique timestamp per url
+# else there are conflicts with reservations
 i = 1
 for room in rooms:
     for month in range(firstMonth, lastMonth):
@@ -44,9 +52,9 @@ for room in rooms:
         print("Reservations for {} {}, room {}".format(frMonth, year, room))
 
         c = calendar.monthcalendar(year, month)
-        tuesdays = [x[weekday] for x in c if x[weekday] != 0]
+        days = [x[weekday] for x in c if x[weekday] != 0]
 
-        for day in tuesdays:
+        for day in days:
             print('{}\t{}{}+{}+{}+{}+{}+{}.{}+{}'.format(day, baseURL, day, month, year, frWeekday, frMonth, time.strftime("%Y%m%d%H%M%S"), str(i).zfill(5), room))
             i += 1
 
