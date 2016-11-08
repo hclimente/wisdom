3e184c8fbe2a07c87984bb8a23a2d82097254565
Hector Climente <hclimente@gmail.com>
2016-11-08 10:38:07 +0100

    use convenient package tidyverse

--- r/data_analysis_environment.R
+++ r/data_analysis_environment.R
@@ -1,15 +1,1 @@
-# convenient for reading data
-library(readr)
-
-# faster, more comfortable data frames
-library(data.table)
-
-# clean data easily
-library(tidyr)
-
-# manipulate data
-library(dplyr)
-library(magrittr)
-
-# plot
-library(ggplot2)
+library(tidyverse)
